4a12bb9a82c2638eaff3cae597e7fe82f4677bbc
bskinny129 <brian.skinner9@gmail.com>
2013-09-06 10:42:36 -0700

    Create coin flip.py

---
+++ Day-3/finished/coin flip.py
@@ -0,0 +1,24 @@
+import random
+
+total_heads = 0
+total_tails = 0
+
+#create a function that half the time returns "heads" and half the time returns "tails"
+def flip_coin():
+    random_number = random.randint(1,2)
+    if random_number == 1:
+        return "heads"
+    else:
+        return "tails"
+
+#flip 100 times
+for n in range(0,1000):
+    flip = flip_coin()
+    if flip == "heads":
+        total_heads = total_heads + 1
+    else:
+        total_tails = total_tails + 1
+
+print "Heads flipped: ", total_heads
+print "Tails flipped: ", total_tails
+

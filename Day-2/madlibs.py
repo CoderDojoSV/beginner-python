b9d02f523dd4b5138b7dc75d919c111efc5b24c9
bskinny129 <brian.skinner9@gmail.com>
2013-09-06 09:46:49 -0700

    Create madlibs.py

---
+++ Day-2/madlibs.py
@@ -0,0 +1,18 @@
+#Madlibs credit: http://www.teach-nology.com/worksheets/language_arts/madlibs/1/
+
+#Print an introduction
+
+#Ask the user for all the variables
+#Have to set: adjective1 through 5, animal1, animal2, food1, food2, verb, ptverb (apast tense verb)
+adjective1 = raw_input("Enter an adjective: ")
+
+#print out the result
+print "" #blank line
+print "Today I went to the zoo. I saw a", adjective1, animal1, "jumping"
+print "up and down in its tree. He",ptverb, "through the large"
+print "tunnel that led to its", adjective2, "cage. I got a", food1
+print "and passed it through the cage to a gigantic", adjective3, animal2
+print "towering over my head. Feeding that animal made me hungry. "
+print "I went to get a", adjective4,"scoop of", food2 +". Afterwards I had to"
+print verb, "to catch our bus. It was a", adjective5, "day at the zoo!"
+

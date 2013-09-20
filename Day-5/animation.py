53b85257c9c90ea67aac101fddb3bc035d0f60a6
bskinny129 <brian.skinner9@gmail.com>
2013-09-19 22:31:27 -0700

    Create animation.py

---
+++ Day-5/animation.py
@@ -0,0 +1,42 @@
+# Listing_16-17.py
+# Copyright Warren Sande, 2009
+# Released under MIT license   http://www.opensource.org/licenses/mit-license.php
+# Version 61  ----------------------------
+
+#Changes by Brian Skinner, Sept-2013
+
+# move a beach ball image in a pygame window with wrapping
+
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+white = [255, 255, 255]
+
+#loads the beach ball image (must be saved in the same place as this file
+my_ball = pygame.image.load('beach_ball.png')
+#set variables to use in our program
+x = 50
+y = 50
+x_speed = 5
+
+while True:
+    #this checks if you've exited the game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            #sys.exit()
+
+    #pause for 20 milliseconds  
+    pygame.time.delay(20)
+    #make the screen completely white
+    screen.fill(white)
+
+    #the animation logic
+    x = x + x_speed
+    if x > screen.get_width():                                
+        x = 0
+
+    #draw the ball on the screen at the x and y location
+    screen.blit(my_ball, [x, y])
+    #update the entire display
+    pygame.display.update()

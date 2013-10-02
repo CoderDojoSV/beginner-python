e18b8c2e1134ec90a0ea970eb8afd4e9b5bf9815
Warren Sande <warren.sande@gmail.com>
2013-10-01 20:21:00 -0700

    Day 2 - Change text to match presentation

--- Day-2/finished/silly sentence generator.py
+++ Day-2/finished/silly sentence generator.py
@@ -1,8 +1,8 @@
 import random
 
-verb_list = ["runs","jumps","climbs"] #create a list of all your verbs
-noun_list = ["cat", "dog","sheep","lamp","mother","father"]
-adjective_list = ["smart","shy","tall","short","happy"]
+verb_list = ["ran","jumped","climbed","licked","programmed"] #create a list of all your verbs
+noun_list = ["pet duck","tree","toaster","computer"]
+adjective_list = ["tasty","large","smelly","green"]
 
 #this function is given a list of words and selects one at random
 def random_word(list_of_words):
@@ -11,13 +11,16 @@ def random_word(list_of_words):
     selected_word = list_of_words[random_word_number] #select the word at the number spot
     return selected_word #hand it back
 
+def random_word2(list_of_words):
+    return random.choice(list_of_words)
+
 #a simple sentence structure:
 #The <adjective> <noun> <verb> the <adjective> <noun>.
 
-adjective1 = random_word(adjective_list)
-noun1 = random_word(noun_list)
-verb = random_word(verb_list) #call the function and remember the result
-adjective2 = random_word(adjective_list)
-noun2 = random_word(noun_list)
-
-print "The", adjective1, noun1, verb, "the", adjective2, noun2
+for i in range(10):
+    adjective1 = random_word2(adjective_list)
+    adjective2 = random_word2(adjective_list)
+    noun1 = random_word2(noun_list)
+    noun2 = random_word2(noun_list)
+    verb = random_word2(verb_list) #call the function and remember the result
+    print "The",adjective1,noun1,verb,"the",adjective2,noun2

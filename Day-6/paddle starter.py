0c72695fcccaf55b26fa680aaa87fe16d4ca54a0
bskinny129 <brian.skinner9@gmail.com>
2013-10-10 13:03:31 -0700

    Create paddle starter.py

---
+++ Day-6/paddle starter.py
@@ -0,0 +1,77 @@
+#Credit the Invent With Python book (http://inventwithpython.com)
+#for doRectsOverlap and isPointInsideRect functions
+
+def doRectsOverlap(rect1, rect2):
+    for a, b in [(rect1, rect2), (rect2, rect1)]:
+        # Check if a's corners are inside b
+        if ((isPointInsideRect(a.left, a.top, b)) or
+            (isPointInsideRect(a.left, a.bottom, b)) or
+            (isPointInsideRect(a.right, a.top, b)) or
+            (isPointInsideRect(a.right, a.bottom, b))):
+            return True
+
+    return False
+
+def isPointInsideRect(x, y, rect):
+    if (x > rect.left) and (x < rect.right) and (y > rect.top) and (y < rect.bottom):
+        return True
+    else:
+        return False
+
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+black = [0, 0, 0]
+
+#the game's variables
+ball_x = 50
+ball_y = 50
+ball_radius = 10
+ball_color = [222,50,50]
+ball_speed = 5
+
+paddle_x = 200
+paddle_y = 400
+paddle_width = 60
+paddle_height = 20
+paddle_color = [20,180,180]
+paddle_speed = 20
+
+while True:
+    for event in pygame.event.get():
+        #check if you've exited the game
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            #sys.exit()
+
+        #check if you pressed a key
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                paddle_x = paddle_x - paddle_speed
+            if event.key == pygame.K_RIGHT:
+                paddle_x = paddle_x + paddle_speed
+
+    #pause for 20 milliseconds  
+    pygame.time.delay(20)
+    #make the screen completely white
+    screen.fill(black)
+
+    #move the ball
+    ball_y = ball_y + ball_speed
+    #check if the ball is off the bottom of the screen
+    if ball_y > screen.get_width():
+        ball_y = 0
+
+    #create rectangles around ball and paddle
+    ball_rect = pygame.Rect(ball_x, ball_y, ball_radius*2,ball_radius*2)
+    paddle_rect = pygame.Rect(paddle_x, paddle_y, paddle_width, paddle_height)
+    #see if the rectangles overlap
+    if doRectsOverlap(ball_rect, paddle_rect):
+        print "YES"
+    
+
+    #draw everything on the screen
+    pygame.draw.circle(screen, ball_color, [ball_x, ball_y], ball_radius, 0)
+    pygame.draw.rect(screen, paddle_color, [paddle_x, paddle_y, paddle_width, paddle_height], 0)
+    #update the entire display
+    pygame.display.update()

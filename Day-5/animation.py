d7d4f38a86df10481308cd92b57e49b0fe942aee
bskinny129 <brian.skinner9@gmail.com>
2014-03-04 19:07:10 -0800

    Update animation.py

--- Day-5/animation.py
+++ Day-5/animation.py
@@ -12,7 +12,7 @@ pygame.init()
 screen = pygame.display.set_mode([640,480])
 white = [255, 255, 255]
 
-#load the beach ball image (must be saved in the same place as this file
+#load the beach ball image (must be saved in the same place as this file)
 my_ball = pygame.image.load('beach_ball.png')
 #set variables to use in our program
 x = 50
@@ -25,17 +25,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #pause for 20 milliseconds  
-    pygame.time.delay(20)
-    #make the screen completely white
-    screen.fill(white)
+    pygame.time.delay(20)  #pause for 20 milliseconds  
+
+    screen.fill(white)  #make the screen completely white
 
     #the animation logic
     x = x + x_speed
     if x > screen.get_width():                                
         x = 0
         
-
     #draw the ball on the screen at the x and y location
     screen.blit(my_ball, [x, y])
     #update the entire display

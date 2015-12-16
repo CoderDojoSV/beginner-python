12ec571b045c46e9000b07ed7d724f821dbc7518
bskinny129 <brian.skinner9@gmail.com>
2015-12-15 17:06:10 -0800

    Update random drawing.py

--- Day-4/random drawing.py
+++ Day-4/random drawing.py
@@ -1,23 +1,30 @@
+#setup
 import random
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((640,480))
 
+#functions
 def random_color():
-    colorname = random.choice(pygame.color.THECOLORS.keys())
-    return pygame.color.THECOLORS[colorname]
+    red = random.randint(0,255)
+    green = random.randint(0,255)
+    blue = random.randint(0,255)
+    return (red, green, blue)
+
 def random_coordinates():
     x = random.randint(0,640)
     y = random.randint(0,480)
     return (x,y)
 
-screen.fill((0,0,0))
+screen.fill(pygame.color.THECOLORS['black'])
 
 for i in range(50):
     pygame.draw.line(screen,random_color(),random_coordinates(),random_coordinates())
 
-pygame.display.flip()
+pygame.display.flip() #updates the display
 
+
+#closing the window
 running = True
 while running:
     for event in pygame.event.get():
